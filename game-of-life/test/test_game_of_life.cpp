64eca14575e1a2cd2a9c55f97013fcf9a949ffb3
HackXIt <hackxit@gmail.com>
2024-12-21 18:11:25 +0100

    Add test files from course

--- game-of-life/test/test_game_of_life.cpp
+++ game-of-life/test/test_game_of_life.cpp
@@ -62,7 +62,21 @@ INSTANTIATE_TEST_SUITE_P(
     GameOfLifeEndToEndTests,
     EndToEndTest,
     ::testing::Values(
-        EndToEndTestParams{"test_input_1.txt", "test_expected_1.txt"},
-        EndToEndTestParams{"test_input_2.txt", "test_expected_2.txt"}
+        EndToEndTestParams{"random250_in.gol", "random250_out.gol"},
+        EndToEndTestParams{"random500_in.gol","random500_out.gol"},
+        EndToEndTestParams{"random750_in.gol","random750_out"},
+        EndToEndTestParams{"random1000_in.gol","random1000_out"},
+        EndToEndTestParams{"random1250_in.gol","random1250_out"},
+        EndToEndTestParams{"random1500_in.gol","random1500_out"},
+        EndToEndTestParams{"random1750_in.gol","random1750_out"},
+        EndToEndTestParams{"random2000_in.gol","random2000_out"},
+        EndToEndTestParams{"random3000_in.gol","random3000_out"},
+        EndToEndTestParams{"random4000_in.gol","random4000_out"},
+        EndToEndTestParams{"random5000_in.gol","random5000_out"},
+        EndToEndTestParams{"random6000_in.gol","random6000_out"},
+        EndToEndTestParams{"random7000_in.gol","random7000_out"},
+        EndToEndTestParams{"random8000_in.gol","random8000_out"},
+        EndToEndTestParams{"random9000_in.gol","random9000_out"},
+        EndToEndTestParams{"random10000_in.gol","random10000_out"},
     )
 );
